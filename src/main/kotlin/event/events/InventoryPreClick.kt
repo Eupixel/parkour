03ed6a30465a7c9e710cec492f18b1e922f058d1
aquestry <antonkonig691@gmail.com>
2025-06-05 17:23:52 +0200

    update

--- src/main/kotlin/event/events/InventoryPreClick.kt
+++ src/main/kotlin/event/events/InventoryPreClick.kt
@@ -1,11 +1,9 @@
 package net.eupixel.event.events
 
-import net.eupixel.game.GameManager
 import net.minestom.server.event.inventory.InventoryPreClickEvent
 
 class InventoryPreClick(event: InventoryPreClickEvent) {
     init {
         event.isCancelled = true
-        GameManager.handleItem(event.player, event.clickedItem)
     }
 }
